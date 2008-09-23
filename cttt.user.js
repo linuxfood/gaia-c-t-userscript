508d7e5fd436de9c5e77a57b5b188223964e301a
Aephex <aephex@gmail.com>
2008-09-22 21:44:01 -0400

    inlined cookie plugin

--- cttt.user.js
+++ cttt.user.js
@@ -281,6 +281,61 @@ var reply_count = 0;
 var reply_timer
 function letsJQuery() {
 
+/**
+ * Cookie plugin
+ *
+ * Copyright (c) 2006 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+
+$.cookie = function(name, value, options) {
+    if (typeof value != 'undefined') { // name and value given, set cookie
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+        }
+        // CAUTION: Needed to parenthesize options.path and options.domain
+        // in the following expressions, otherwise they evaluate to undefined
+        // in the packed version for some reason...
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else { // only name given, get cookie
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = $.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
+
+
+
 /* ========================================
  *
  *  jQuery Timer plugin v0.1
@@ -360,7 +415,6 @@ function letsJQuery() {
  
 // Step 1, grab jquery & plugin libraries
 injectScript(repoURL('plugins/jquery.js'));
-injectScript(repoURL('plugins/jquery.cookie.js'));
 injectScript(repoURL('plugins/jquery.scrollTo-1.4.0.js'));
 injectScript(repoURL('plugins/jquery.signature.js')); // Not sure if plugins/ was a good spot, oh well.
 injectCss()
